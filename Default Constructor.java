8b5e7e9abbb913ee4bf24b2b216ae12e48bb970f
Yogaprasad  M K <120255515+Yogaprasadmk@users.noreply.github.com>
2024-05-28 23:32:40 +0530

    Default Parameter

---
+++ Default Constructor.java
@@ -0,0 +1,16 @@
+
+
+// constructor
+class Constructors{
+    Constructors(){
+        System.out.println("Default Parameter");
+    }
+}
+
+
+public class Main{
+
+    public static void main (String[] args) {
+        Constructors A = new Constructors();
+    }
+}
